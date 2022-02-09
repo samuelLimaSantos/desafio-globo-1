7154e52cae9dfb1ffa88ba2b467e6f889b06f15b
Samuel Santos <samuellima280499@gmail.com>
2022-02-09 20:04:00 -0300

    Add swagger to documentate endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import { routes } from './routes';
+import swaggerApi from "swagger-ui-express";
+import documentationFile from './shared/documentation/swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1", swaggerApi.serve, swaggerApi.setup(documentationFile));
+
 app.use(routes);
 
 export { app };
